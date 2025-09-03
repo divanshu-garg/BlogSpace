79d11fbf36a78a4131b446fb76576fcb69ca4ee0
Divanshu Garg <divanshugarg49@gmail.com>
2025-09-03 13:56:33 +0530

    created appwrite authentication service

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  console.log();
+  
+  return (
+      <>
+      <h1>blog project in react and appwrite with redux as well</h1>
+      </>
+  )
+}
+
+export default App
---
+++ src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setProject(conf.appwriteProjectId)
+      .setEndpoint(conf.appwriteUrl);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ name, email, password }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // login function runs here
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const loggedInUser = await this.account.createEmailPasswordSession(
+        email,
+        password
+      );
+      return loggedInUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("error occured while getting current user:", error);
+      return null; // not throwing error, so return to stop execution
+    }
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("error occured while logging out current user:", error);
+      return null; // not throwing error, so return to stop execution        }
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
