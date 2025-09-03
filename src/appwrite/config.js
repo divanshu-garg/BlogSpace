18fa1d9e2baef73d90823271fc4b5adfa9a71523
Divanshu Garg <divanshugarg49@gmail.com>
2025-09-03 14:49:25 +0530

    created database service for backend via appwrite

---
+++ src/appwrite/config.js
@@ -0,0 +1,96 @@
+import conf from "../conf/conf";
+import { Client, ID, TablesDB, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  tablesDB;
+  bucket;
+  constructor() {
+    this.client
+      .setProject(conf.appwriteProjectId)
+      .setEndpoint(conf.appwriteUrl);
+    this.tablesDB = new TablesDB(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.tablesDB.createRow({
+        databaseId: conf.appwriteDatabaseId,
+        tableId: conf.appwriteTableId,
+        rowId: slug,
+        data: {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        },
+      });
+    } catch (error) {
+      console.log("error occured while creating post: ", error);
+      return null;
+    }
+  }
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.tablesDB.updateRow({
+        databaseId: conf.appwriteDatabaseId,
+        tableId: conf.appwriteTableId,
+        rowId: slug,
+        data: {
+          title,
+          content,
+          featuredImage,
+          status,
+        },
+      });
+    } catch (error) {
+      console.log("error occured while updating post: ", error);
+      return null;
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.tablesDB.deleteRow({
+        databaseId: conf.appwriteDatabaseId,
+        tableId: conf.appwriteTableId,
+        rowId: slug,
+      });
+      return true;
+    } catch (error) {
+      console.log("error occured while deleting post: ", error);
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.tablesDB.getRow({
+        databaseId: conf.appwriteDatabaseId,
+        tableId: conf.appwriteTableId,
+        rowId: slug,
+      });
+    } catch (error) {
+      console.log("error occured while fetching all posts: ", error);
+      return false;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.tablesDB.listRows({
+        databaseId: conf.appwriteDatabaseId,
+        tableId: conf.appwriteTableId,
+        queries: queries,
+      });
+    } catch (error) {
+      console.log("error occured while fetching all active posts: ", error);
+      return false;
+    }
+  }
+}
+
+const service = new Service();
+export default service;
